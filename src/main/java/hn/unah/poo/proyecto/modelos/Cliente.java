cef68c7b58dfb7bc130e644d03e13adfbe08ad0a
rigo <rguillen.jose@gmail.com>
2024-11-29 17:57:01 -0600

    c

---
+++ src/main/java/hn/unah/poo/proyecto/modelos/Cliente.java
@@ -0,0 +1,31 @@
+package hn.unah.poo.proyecto.modelos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "proyecto_poo")
+public class Cliente {
+    
+    @Id
+    private String dni;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String telefono;
+
+    private String correo;
+
+    private double sueldo;
+}
