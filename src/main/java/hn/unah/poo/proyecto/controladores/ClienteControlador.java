d248d595110c8bf0c5b0811e512e0809e1d75cc5
rigo <rguillen.jose@gmail.com>
2024-12-01 23:29:30 -0600

    swagger

--- src/main/java/hn/unah/poo/proyecto/controladores/ClienteControlador.java
+++ src/main/java/hn/unah/poo/proyecto/controladores/ClienteControlador.java
@@ -13,26 +13,33 @@ import org.springframework.web.bind.annotation.RestController;
 import hn.unah.poo.proyecto.dtos.ClienteDTO;
 import hn.unah.poo.proyecto.modelos.Cliente;
 import hn.unah.poo.proyecto.servicios.ClienteServicios;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("api/cliente")
+@Tag(name = "Cliente", description = "Metodos relacionados con Clientes")
 public class ClienteControlador {
 
     @Autowired
     private ClienteServicios clienteServicios;
 
     @PostMapping("/crear/nuevo")
+    @Operation(summary = "Crea un nuevo Cliente", description = "Permite crear un cliente con todas sus caracteristicas.")
     public String crearNuevoCliente(@RequestBody ClienteDTO dni) {
         return this.clienteServicios.crearCliente(dni);
     }
     
-     @GetMapping("/obtener/dni")
+    
+    @GetMapping("/obtener/dni")
+    @Operation(summary = "Obtiene un cliente por su nmero de DNI", description = "Este retorna todos los dettales de nuestro Cliente el cual buscamos por su DNI")
     public Cliente obtenerPorId(@RequestParam(name="dni") String dni) {
         return this.clienteServicios.obtenerPorDni(dni);
         
 }
 
     @GetMapping("/obtener")
+    @Operation(summary = "Este metodo obtiene todos los clientes y sus datos", description = "Nos retorna todos los clientes y sus Datos como direccion, prestamos,etc")
         public List<Cliente> obtenerTodos(){
         return this.clienteServicios.obtenerTodos();
         }
